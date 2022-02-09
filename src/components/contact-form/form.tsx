9d30da9fec099ff815484d9ecc7a6ffb60801f87
Petruta Raul <petruta.raul98@gmail.com>
2022-02-09 15:43:39 +0200

    convert form to reactive-form

--- src/components/contact-form/form.tsx
+++ src/components/contact-form/form.tsx
@@ -2,63 +2,71 @@ import React, { useState } from "react";
 import "./form.scss";
 import { send } from "emailjs-com";
 import { serviceID, templateID, userID } from "./variables/variables";
+import {
+  FormBuilder,
+  FieldGroup,
+  FieldControl,
+  Validators,
+} from "react-reactive-form";
+import TextInput from "./text-input/text-input";
 
 function ContactForm() {
-  const [toSend, setToSend] = useState({
-    from_name: "",
-    message: "",
-    your_email: "",
+  const [mailFailed, setMailFailed] = useState(false);
+
+  const loginForm = FormBuilder.group({
+    from_name: ["", Validators.required],
+    message: ["", Validators.required],
+    your_email: ["", [Validators.required, Validators.email]],
   });
 
   const onSubmit = (e: any) => {
     e.preventDefault();
-    send(serviceID, templateID, toSend, userID)
+    send(serviceID, templateID, loginForm.value, userID)
       .then((response) => {
+        loginForm.reset();
         console.log("SUCCESS!", response.status, response.text);
-        setToSend({
-          from_name: "",
-          message: "",
-          your_email: "",
-        });
       })
       .catch((err) => {
         console.log("FAILED...", err);
+        setMailFailed(true);
       });
-  };
-
-  const handleChange = (e: any) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
   };
 
   return (
     <div className="app-contactform">
-      <form onSubmit={onSubmit}>
-        <h1>
-          Send us an email <br /> to get a demo
-        </h1>
-        <input
-          type="text"
-          name="from_name"
-          placeholder="From name"
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="Your message"
-          value={toSend.message}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="your_email"
-          placeholder="Your email"
-          value={toSend.your_email}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <FieldGroup
+        control={loginForm}
+        render={({ invalid }) => (
+          <form onSubmit={onSubmit}>
+            <h1>
+              Send us an email <br /> to get a demo
+            </h1>
+            <FieldControl
+              name="from_name"
+              render={TextInput}
+              meta={{ label: "From name" }}
+            />
+
+            <FieldControl
+              name="message"
+              render={TextInput}
+              meta={{ label: "Your message" }}
+            />
+
+            <FieldControl
+              name="your_email"
+              render={TextInput}
+              meta={{ label: "Your email" }}
+            />
+            <button type="submit" disabled={invalid}>
+              Submit
+            </button>
+          </form>
+        )}
+      />
+      {mailFailed && (
+        <span className="app-contactform__failed">Mail failed!</span>
+      )}
     </div>
   );
 }
