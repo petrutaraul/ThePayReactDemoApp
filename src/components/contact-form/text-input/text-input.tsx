56bdca333aa226a1364f8e6e06a7ab293fe841c3
Petruta Raul <petruta.raul98@gmail.com>
2022-02-09 15:43:58 +0200

    convert form to reactive-form

---
+++ src/components/contact-form/text-input/text-input.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./text-input.scss";
+
+function TextInput({
+  handler,
+  touched,
+  hasError,
+  meta,
+}: {
+  handler: any;
+  touched: boolean;
+  hasError: any;
+  meta: any;
+}) {
+  return (
+    <div className="app-text-input">
+      <input placeholder={`Enter ${meta.label}`} {...handler()} />
+      <span>
+        {touched && hasError("required") && `${meta.label} is required!`}
+      </span>
+    </div>
+  );
+}
+
+export default TextInput;
